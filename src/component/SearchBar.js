0135f2321b870615384cd75a81a6234f63e3a045
EslamSedky <eslam.sedky@roqay.com>
2024-03-19 12:39:36 +0200

    First commit

---
+++ src/component/SearchBar.js
@@ -0,0 +1,43 @@
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+const SearchBar = ({term, onTermChange}) => {
+  return (
+    <View style={style.backgroundStyle}>
+      <Icon
+        name="search"
+        size={30}
+        color="black"
+        style={style.backgroundIcon}
+      />
+      <TextInput
+        placeholder="Search"
+        style={style.inputStyle}
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={term}
+        onChangeText={newTerm => onTermChange(newTerm)}
+      />
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  backgroundStyle: {
+    marginTop: 15,
+    backgroundColor: 'gray',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 16,
+    flexDirection: 'row',
+  },
+  inputStyle: {
+    flex: 1,
+  },
+  backgroundIcon: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15,
+  },
+});
+
+export default SearchBar;
